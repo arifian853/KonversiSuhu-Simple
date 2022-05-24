e3426dff33a32c0cc5f411482ac12e2eaf429576
arifian853 <arifiansaputra43@gmail.com>
2022-05-24 19:55:11 +0700

    Added popup

--- script.js
+++ script.js
@@ -53,4 +53,28 @@ fahrenheitInput.addEventListener('input', fahrenheitToAll);
 kelvinInput.addEventListener('input', kelvinToAll);
 reaumurInput.addEventListener('input', reaumurToAll);
 
+
+
+//Pop up modal
+
+var modal = document.getElementById("MainPopUp");
+
+var btn = document.getElementById("ButtonPopUp");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
 //Copyright © Arifian Saputra , 2021. All rights reserved.
